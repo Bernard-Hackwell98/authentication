c4d67b459e78c9b2f2a978ee619c31f2fa63e6a3
Bernard-Hackwell98 <143324911+Bernard-Hackwell98@users.noreply.github.com>
2025-02-03 15:17:11 +0500

    initial

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import SessionWrapper from "./component/sessionwrapper";
 
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
